2380e8cf2bc1510841d901499fd75e03e6435f1f
hsingla005 <hsingla@twilio.com>
2023-05-08 09:44:53 +0530

    FLEXY-4441 Kibana intergration for logging

--- ui-src/src/utils/ErrorManager.ts
+++ ui-src/src/utils/ErrorManager.ts
@@ -1,3 +1,8 @@
+import * as Flex from '@twilio/flex-ui';
+import packageJSON from '../../package.json';
+
+const flexManager = window?.Twilio?.Flex?.Manager?.getInstance();
+
 export enum FlexPluginErrorType {
     action = "ActionFramework",
     serverless = "Serverless",
@@ -43,6 +48,18 @@ class ErrorManagerImpl {
     public processError(error: FlexPluginError, showNotification: boolean): FlexPluginError {
         try {
             console.log(`Supervisor barge-in coach Plugin: ${error}\nType: ${error.content.type}\nContext:${error.content.context}`);
+            const pluginError = new Flex.FlexError(error.message, {
+                plugin: { name: packageJSON.name, version: packageJSON.version },
+                description: error.content.description,
+              });
+              if (flexManager?.reportErrorEvent) {
+                flexManager.reportErrorEvent(pluginError);
+              }
+              if (showNotification) {
+                Flex.Notifications.showNotification('ErrorSupervisorBargeCoach', {
+                  error: error,
+                });
+              }
         } catch (e) {
             // Do not throw, let's avoid Inceptions
         }
